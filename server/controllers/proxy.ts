ab1654eaa2175dd9582a608a0e159fe538a55c1b
Pavel <palllkaignatev@gmail.com>
2024-04-22 23:26:00 +0500

    dsadsa

--- server/controllers/proxy.ts
+++ server/controllers/proxy.ts
@@ -25,28 +25,28 @@ router.get("/:accountId", async (req, res) => {
     const { accountId } = req.params;
     const collection = (await DB()).collection("proxy");
 
-    let result = await collection.findOne(
+    const result = await collection.findOne(
       {
         accountId,
       },
       { projection: { database: 0 } }
     );
 
-    if (result) {
-      res.send(result || {}).status(200);
-      return;
-    }
+    // if (result) {
+    //   res.send(result || {}).status(200);
+    //   return;
+    // }
 
-    const proxy = await collection.findOne({
-      accountId: { $exists: false },
-    });
-    if (proxy) {
-      await collection.updateOne({ _id: proxy._id }, { $set: { accountId } });
-    }
+    // const proxy = await collection.findOne({
+    //   accountId: { $exists: false },
+    // });
+    // if (proxy) {
+    //   await collection.updateOne({ _id: proxy._id }, { $set: { accountId } });
+    // }
 
-    if (proxy) {
-      result = proxy;
-    }
+    // if (proxy) {
+    //   result = proxy;
+    // }
 
     res.send(result || {}).status(200);
   } catch (e: any) {
